a4c299ecb1c4a3c0f674dcc8e8abd496e0b27ad4
suresheeshan <suresh.nssy9@gmail.com>
2023-07-05 12:09:04 +0530

    new files

---
+++ src/main/java/com/javalabs/tech/SpringCurdServiceApplication.java
@@ -0,0 +1,13 @@
+package com.javalabs.tech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCurdServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCurdServiceApplication.class, args);
+	}
+
+}
